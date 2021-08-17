ff9f4f0e76b029e829783e7d9b81cbf1b8b00c5b
Jake Egelberg <jake1egelberg@gmail.com>
2021-08-16 23:35:37 -0400

    Add files via upload

--- 0INSTALLATION.bat
+++ 0INSTALLATION.bat
@@ -1,2 +1,2 @@
 @echo on
-D:/RNASeqV2-main/R-4.1.1/bin/x64/R.exe CMD BATCH D:/RNASeqV2-main/scripts/0installation.R
+C:/RNASeqV2-main/R-4.1.1/bin/x64/R.exe CMD BATCH C:/RNASeqV2-main/scripts/0installation.R
