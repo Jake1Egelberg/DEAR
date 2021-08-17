8ff871b6cbd1f2583c453d17397ffc37b8cf4342
Jake Egelberg <jake1egelberg@gmail.com>
2021-08-17 16:55:54 -0400

    Update 0INSTALLATION.bat

--- 0INSTALLATION.bat
+++ 0INSTALLATION.bat
@@ -1,2 +1,2 @@
 @echo on
-C:/RNASeqV2-main/R-4.1.1/bin/x64/R.exe CMD BATCH C:/RNASeqV2-main/scripts/0installation.R
+C:/DEAR-main/R-4.1.1/bin/x64/R.exe CMD BATCH C:/DEAR-main/scripts/0installation.R
